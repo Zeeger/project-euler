755bc1f35d5decc1c4d499f16cbf9555295526c8
Zeeger <akrieger1986@gmail.com>
2012-07-26 13:54:02 -0500

    Solved problem 1 in csharp.

---
+++ csharp-euler/csharp-euler/Problems/Problem00001.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_euler
+{
+	class Problem00001 : ISolveProblems
+	{
+		public void Solve()
+		{
+			long total = 0;
+
+			for (int x = 0; x < 1000; x++)
+			{
+				//Console.WriteLine(x);
+
+				//Console.WriteLine(x % 3);
+
+				if (x % 3 == 0 || x % 5 == 0)
+				{
+					total += x;
+				}
+
+				//Console.WriteLine(total);
+			}
+
+			Console.WriteLine("Answer is " + total);
+		}
+	}
+}
---
+++ csharp-euler/csharp-euler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace csharp_euler
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			var problemList = new List<ISolveProblems>();
+
+			problemList.Add(new Problem00001());
+
+			foreach (var problem in problemList)
+			{
+				problem.Solve();
+
+				var problemClassName = problem.GetType().FullName ;
+
+				Console.WriteLine("Problem " + problemClassName + " solved.");
+
+				Console.ReadLine();
+			}
+		}
+	}
+}
