ab583e7108964261bbcbf9fbe35fa1fc50538913
Zeeger <akrieger1986@gmail.com>
2012-07-30 09:51:20 -0500

    Reworked the problem loading mechanism to be more complete. A list of available problems is now displayed. Choosing one takes you back to the menu after it's solved. Added clear option to menu.

---
+++ csharp-euler/csharp-euler/Problems/Problem00013.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_euler.Problems
+{
+	class Problem00013 : ISolveProblems 
+	{
+		public void Solve()
+		{
+			Console.WriteLine("Insert problem 13 here");
+		}
+	}
+}
--- csharp-euler/csharp-euler/Program.cs
+++ csharp-euler/csharp-euler/Program.cs
@@ -4,16 +4,132 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using csharp_euler.Problems;
+using System.IO;
 
 namespace csharp_euler
 {
 	class Program
 	{
-		
+
 		static void Main(string[] args)
 		{
 			var problemList = new List<ISolveProblems>();
 
+			//LoadProblemsConsecutively(ref problemList);
+
+			Dictionary<int, ISolveProblems> problemDictionary = LoadProblemsByNamespace();
+
+			string input = "";
+
+			do
+			{
+				PrintKeysOfDictionary(problemDictionary);
+
+				Console.WriteLine("Type a problem number to solve, C to clear screen, or Q to quit.");
+
+				input = Console.ReadLine();
+
+				if (input.ToUpper() == "C")
+				{
+					Console.Clear();
+					continue;
+				}
+
+				int problemNumber = -1;
+
+				if (int.TryParse(input, out problemNumber))
+				{
+					try
+					{
+						problemDictionary[problemNumber].Solve();
+					}
+					catch (KeyNotFoundException kex)
+					{
+						Console.WriteLine("That problem was not loaded. Try again...");
+					}
+				}
+
+			} while (input.ToUpper() != "Q");
+		}
+
+		private static void PrintKeysOfDictionary(Dictionary<int, ISolveProblems> problemDictionary)
+		{
+			string keyString = "";
+
+			foreach (var key in problemDictionary)
+			{
+				keyString += key.Key + ", ";
+			}
+
+			keyString = keyString.Substring(0, keyString.Length - 2);
+
+			Console.WriteLine(keyString);
+		}
+
+		private static Dictionary<int,ISolveProblems> LoadProblemsByNamespace()
+		 {
+			string @namespace = "csharp_euler.Problems";
+
+			var problemDictionary = new Dictionary<int,ISolveProblems>();
+
+			var className = from typeName in Assembly.GetExecutingAssembly().GetTypes()
+							where typeName.IsClass && typeName.Namespace == @namespace
+							orderby typeName.Name
+							select typeName;
+
+			var problemsLoaded = "";
+
+			foreach (var type in className)
+			{
+				var currentProblem = InstantiateProblemClass(type.FullName);
+
+				if (currentProblem != null)
+				{
+					//problemList.Add(currentProblem);
+
+					var classNumberString = type.Name.Substring(type.Name.Length - 5, 5);
+
+					var classNumber = int.Parse(classNumberString);
+
+					problemDictionary.Add(classNumber,currentProblem);
+
+					problemsLoaded += classNumber + ", ";
+					//Console.WriteLine(classNumber + ", ");
+				}
+
+			} 
+			
+			problemsLoaded = problemsLoaded.Substring(0, problemsLoaded.Length - 2);
+
+			Console.WriteLine(problemsLoaded);
+
+			return problemDictionary;
+		}
+
+		private static void LoadProblemsByFile(ref List<ISolveProblems> problemList)
+		{
+			string fullPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+			string directoryPath = Path.GetDirectoryName(fullPath);
+
+			string[] problemClassFiles = Directory.GetFiles(directoryPath + @"\Problems\", @"Problem*.cs");
+
+			foreach (var name in problemClassFiles)
+			{
+				Console.WriteLine(name);
+
+			}
+		}
+
+		private static ISolveProblems InstantiateProblemClass(string className)
+		{
+			ISolveProblems problem = Assembly.GetExecutingAssembly().CreateInstance(className) as ISolveProblems;
+
+			return problem;
+		}
+
+		private static void LoadProblemsConsecutively(ref List<ISolveProblems> problemList)
+		{
 			bool classInstantiationSuccess = true;
 			int problemNumber = 1;
 
@@ -21,7 +137,7 @@ namespace csharp_euler
 			{
 				var className = "csharp_euler.Problems.Problem" + problemNumber.ToString().PadLeft(5, '0');
 
-				ISolveProblems problem = Assembly.GetExecutingAssembly().CreateInstance(className) as ISolveProblems;
+				ISolveProblems problem = InstantiateProblemClass(className);
 
 				if (problem != null)
 				{
@@ -35,45 +151,6 @@ namespace csharp_euler
 				problemNumber++;
 			}
 			while (classInstantiationSuccess == true);
-
-			//problemList.Add(new Problem00001());
-			//problemList.Add(new Problem00002());
-			//problemList.Add(new Problem00003()); 
-			//problemList.Add(new Problem00004());
-			//problemList.Add(Assembly.GetExecutingAssembly().CreateInstance("csharp_euler.Problems.Problem00004") as ISolveProblems);
-
-			Console.WriteLine("Start at problem:");
-
-			var input = Console.ReadLine();
-
-			long startingProblem;
-
-			if (!long.TryParse(input, out startingProblem))
-			{
-				startingProblem = 1;
-			}
-
-			for (long x = 0;x<problemList.ToArray().Length;x++)
-			{
-				var problem = problemList.ToArray()[x];
-
-				if (x+1 >= startingProblem)
-				{
-
-					problem.Solve();
-
-					var problemClassName = problem.GetType().FullName;
-
-					Console.WriteLine("Problem " + problemClassName + " solved. Enter 'Q' to exit.");
-
-					var nextInput = Console.ReadLine();
-
-					if (nextInput.ToLower() == "q")
-						break;
-
-					Console.WriteLine();
-				}
-			}
 		}
 	}
 }
